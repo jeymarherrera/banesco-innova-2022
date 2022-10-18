e37436c350e644205cd468a90342a5353d8996e4
jeymarherrera <jeymarherrera@gmail.com>
2022-10-18 12:24:55 -0500

    REGISTRE

--- view/registro.php
+++ view/registro.php
@@ -27,53 +27,82 @@
     <main>
     <div class="wrapper">
       <ul class="steps">
-        <li class="is-active">Step 1</li>
-        <li>Step 2</li>
-        <li>Step 3</li>
+        <li class="is-active">Tipo de Empresa</li>
+        <li>Datos Personales</li>
+        <li>Datos Representantes</li>
+        <li>Datos Establecimiento</li>
+        <li>Sucursales</li>
+        <li>Volumen Ventas</li>
       </ul>
       <form class="form-wrapper">
         <fieldset class="section is-active">
-          <h3>Your Details</h3>
-          <input type="text" name="name" id="name" placeholder="Name">
-          <input type="text" name="email" id="email" placeholder="Email">
+          <h3>Ingrese su tipo de empresa</h3>
+             <!-- validar en php -->
+             <label for="lang">Lenguaje</label>
+      <select name="lenguajes" id="lang">
+        <option value="javascript">JavaScript</option>
+        <option value="php">PHP</option>
+        <option value="java">Java</option>
+        <option value="golang">Golang</option>
+        <option value="python">Python</option>
+        <option value="c#">C#</option>
+        <option value="C++">C++</option>
+        <option value="erlang">Erlang</option>
+      </select>
+      <input type="submit" value="Enviar" />
+
+          <div class="button">Next</div>
+        </fieldset>
+        
+        <fieldset class="section">
+          <h3>Ingrese sus datos personales</h3>
+          <input type="text" name="name" id="name" placeholder="Nombre" required>
+          <input type="text" name="lastname" id="lastname" placeholder="Apellido" required>
+          <input type="text" name="id_cliente" id="id_cliente" placeholder="Cédula o Passaporte" required>
+          <input type="text" name="nacionalidad" id="nacionalidad" placeholder="Nacionalidad" required>
+          <input type="text" name="phone" id="phone" placeholder="Télefono" required>
+          <input type="text" name="email" id="email" placeholder="Correo" required>
+          <!-- PREGUNTAR SERVICIOI -->
+          
+          <!-- <div class="button" type="reset" value="Borrar" >Borrar</div> -->
+          <br>
+          <div class="button">Next</div>
+        </fieldset>
+        
+        <fieldset class="section">
+        <h3>Ingrese los datos del representante</h3>
+          <input type="text" name="name" id="name" placeholder="Nombre" required>
+          <input type="text" name="lastname" id="lastname" placeholder="Apellido" required>
+          <input type="text" name="id_cliente" id="id_cliente" placeholder="Cédula o Passaporte" required>
+          <input type="text" name="ciudad" id="ciudad" placeholder="Ciudadanía" required>
+          <input type="text" name="phone" id="phone" placeholder="Télefono" required>
+          <input type="text" name="email" id="email" placeholder="Correo" required>
+          <!-- <div class="button" type="reset" value="Borrar" >Borrar</div> -->
+          <br>
           <div class="button">Next</div>
         </fieldset>
         <fieldset class="section">
-          <h3>Account Type</h3>
-          <div class="row cf">
-            <div class="four col">
-              <input type="radio" name="r1" id="r1" checked>
-              <label for="r1">
-                <h4>Designer</h4>
-              </label>
-            </div>
-            <div class="four col">
-              <input type="radio" name="r1" id="r2"><label for="r2">
-                <h4>Developer</h4>
-              </label>
-            </div>
-            <div class="four col">
-              <input type="radio" name="r1" id="r3"><label for="r3">
-                <h4>Project Manager</h4>
-              </label>
-            </div>
-          </div>
-          <div class="button">Next</div>
-        </fieldset>
-        <fieldset class="section">
-          <h3>Choose a Password</h3>
-          <input type="password" name="password" id="password" placeholder="Password">
-          <input type="password" name="password2" id="password2" placeholder="Re-enter Password">
-          <input class="submit button" type="submit" value="Sign Up">
-        </fieldset>
-        <fieldset class="section">
-          <h3>Account Created!</h3>
-          <p>Your account has now been created.</p>
+          <h3>Establecimiento</h3>
+          <input type="text" name="Razon_Social" id="RAZON_SOCIAL" placeholder="Razon Social" required>
+          <input type="text" name="RUC_CLIENTE" id="RUC_CLIENTE" placeholder="RUC" required>
+          <input type="text" name="id_cliente" id="id_cliente" placeholder="Cédula o Passaporte" required>
+          <input type="text" name="ciudad" id="ciudad" placeholder="Ciudadanía" required>
+          <input type="text" name="phone" id="phone" placeholder="Télefono" required>
+          <input type="text" name="email" id="email" placeholder="Correo" required>
           <div class="button">Reset Form</div>
         </fieldset>
       </form>
     </div>
   </main>
-
+      <!-- links javascript -->
+      <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="public/lib/wow/wow.min.js"></script>
+    <script src="public/lib/easing/easing.min.js"></script>
+    <script src="public/lib/waypoints/waypoints.min.js"></script>
+    <script src="public/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="public/lib/counterup/counterup.min.js"></script>
+    <script src="public/js/main.js"></script>
+  <script src="public/js/registro.js"></script>
 </body>
 </html>
